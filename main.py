8bc9d098552019d28f488969c0e2ccbc1b6cf984
alyssonmazoni <alysson.mazoni@gmail.com>
2026-01-19 13:19:57 -0300

    Logging to file.

--- main.py
+++ main.py
@@ -4,11 +4,6 @@ if __name__ == "__main__":
 
 
     job_oa = prepare_job('works', 'work')
-
-    #print(job_oa)
     job_oa = read_job_status('works')
     download_upload(job_oa)
-
-    #print(job_oa)
-
  
